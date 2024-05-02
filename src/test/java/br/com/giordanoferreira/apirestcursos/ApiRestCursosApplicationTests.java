62203442bb55f69cb4c5dc6a978471ff8b74541e
Giordano <giordanopalmezano@gmail.com>
2024-05-02 00:38:19 -0300

    initial commit

---
+++ src/test/java/br/com/giordanoferreira/apirestcursos/ApiRestCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.giordanoferreira.apirestcursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
