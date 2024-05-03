0cc75617f0cb5a34a524c2625efbcfd9d8a62d7d
Giordano <giordanopalmezano@gmail.com>
2024-05-03 17:19:34 -0300

    FEAT: Update README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# API-REST-Plataforma-Cursos
+<h1 align="center" style="font-weight: bold;">API REST Plataforama Cursos 💻</h1>
+
+<p align="center">
+    <a href="#tech">Tecnologias</a>
+</p>
+
+<p align="center">
+    <b>Projeto de uma API Restful para uma plataforma de venda de cursos de tecnologia que desenvolvi para o meu trabalho de TCC no curso de Sistemas de Informação.</b>
+</p>
+
+<h2 id="technologies">💻 Tecnologias</h2>
+- Intellij IDEA
+- Java 17
+- Spring Boot 3
+- PostgreSQL
+- Postman
+- Swagger
 
--- src/main/java/br/com/giordanoferreira/apirestcursos/service/UsuarioService.java
+++ src/main/java/br/com/giordanoferreira/apirestcursos/service/UsuarioService.java
@@ -9,6 +9,7 @@ import br.com.giordanoferreira.apirestcursos.model.entity.Usuario;
 import br.com.giordanoferreira.apirestcursos.model.exception.UsuarioNaoEncontradoException;
 import br.com.giordanoferreira.apirestcursos.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
